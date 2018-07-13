ac75e74c8c648d7d8aaaa607d753c2901ad9434f
powerbackso <543004225@qq.com>
2018-07-13 10:19:59 +0800

    老王的代码

--- maven_ssm_utils/src/main/java/cn/pbs/utils/DateUtil.java
+++ maven_ssm_utils/src/main/java/cn/pbs/utils/DateUtil.java
@@ -1,4 +1,4 @@
-package cn.pbs.utils;
+﻿package cn.pbs.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -6,6 +6,7 @@ import java.util.Date;
 public class DateUtil  {
 	public static String dateToString(Date date) {
 		//使用SimpleDateFormat格式化传递进来的date参数
+//使用SimpleDateFormat格式化传递进来的date参数
 		SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return sd.format(date);
 	}
